9715b83727387a9991b69264c02526733e8100a3
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-04-20 17:18:39 +0530

    // Liner search in array

---
+++ 06.cpp
@@ -0,0 +1,16 @@
+// Liner search in array    
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1, 3, 6, 8, 7, 9, 2, 4, 5};
+    int target = 7;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] == target){
+            cout << i << endl;
+            break;
+        }
+    }
+
+}
