e94ec3e0e4749717551a959eded7252b1b079e63
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-04-20 17:15:56 +0530

    01.cpp
    // larest no in array

---
+++ 01.cpp
@@ -0,0 +1,18 @@
+// larest no in array
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {2,8,3,5,7,4,8};
+    int n = (sizeof(arr)/ sizeof(0)) ;
+    
+    int max = INT_MIN;
+    for(int i = 0; i < n; i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+
+    cout << "maximum no in the array is = " << max << endl;
+
+}
+
